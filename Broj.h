025b1f4807978869db30e121d222770394424c5a
njoffra <markic.franjo.21@size.ba>
2022-12-15 14:46:30 +0100

    Upladovo kviz

---
+++ Broj.h
@@ -0,0 +1,20 @@
+//---------------------------------------------------------------------------
+
+#ifndef BrojH
+#define BrojH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+//---------------------------------------------------------------------------
+class TBrojForma : public TForm
+{
+__published:	// IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+	__fastcall TBrojForma(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TBrojForma *BrojForma;
+//---------------------------------------------------------------------------
+#endif
